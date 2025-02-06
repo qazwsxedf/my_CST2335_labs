00003c58758bc3c6fcf490e0d0d83d0da7ee87ce
Sagan Joshi <saganjoshi@gmail.com>
2025-02-06 17:47:03 -0500

    Added Alert Dialog and SnackBack and utilized EncryptedSharedPreferences to save the username and password used if the user wants.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
 import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -35,14 +37,16 @@ class _MyHomePageState extends State<MyHomePage> {
 
   late TextEditingController _loginController; // controller for login field
   late TextEditingController _passwordController; // controller for password field
-  String _imagePath = "images/question-mark.png"; // String variable to store image path
-  final String _correctPassword = "QWERTY123"; // Correct password to compare user input with
+  late EncryptedSharedPreferences prefs;
+  late SnackBar snackBar;
 
   @override
   void initState() {
     super.initState();
     _loginController = TextEditingController();
     _passwordController = TextEditingController();
+    prefs = EncryptedSharedPreferences();
+    _loadSavedCredentials();
   }
 
   @override
@@ -52,19 +56,54 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  // If user input is same as the correct password, image path is set to the
-  //light bulb image, else it is set to the stop sign image.
-  void _checkPassword(){
-    if(_passwordController.text == _correctPassword){
-      setState(() {
-        _imagePath = "images/idea.png";
-      });
-    } else {
-      setState(() {
-        _imagePath = "images/stop.png";
-      });
-    }
+  void _loadSavedCredentials() async {
+    var savedUsername = await prefs.getString('username');
+    var savedPassword = await prefs.getString('password');
 
+    if (savedPassword != '' && savedUsername != '') {
+      _loginController.text = savedUsername;
+      _passwordController.text = savedPassword;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Previous login credentials loaded.'),
+          action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _loginController.text = '';
+                _passwordController.text = '';
+              });
+            },
+          ),
+        ),
+      );
+    }
+  }
+
+  void _showAlertDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text("Save Login Credentials"),
+        content: const Text("Would you save your login name and password for next time?"),
+        actions: [
+          TextButton(onPressed: () async {
+            String username = _loginController.text.isEmpty ? '' : _loginController.text;
+            String password = _passwordController.text.isEmpty ? '' : _passwordController.text;
+
+            await prefs.setString('username', username);
+            await prefs.setString('password', password);
+            Navigator.pop(context);
+          }, child: Text("Yes")),
+          TextButton(onPressed: () async {
+            await prefs.remove('username');
+            await prefs.remove('password');
+            Navigator.pop(context);
+          }, child: Text("No"))
+        ],
+      ),
+    );
   }
 
   @override
@@ -86,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
             TextField(
               controller: _loginController,
               decoration: InputDecoration(
-                hintText: "Login",
+                hintText: "Username",
                 border: OutlineInputBorder(),
               ),
             ),
@@ -100,9 +139,7 @@ class _MyHomePageState extends State<MyHomePage> {
               obscureText: true,
             ),
             // on button press, the _checkPassword function is called
-            ElevatedButton(onPressed: _checkPassword, child: Text("Login")),
-            // displays the image based on what is stored in _imagePath variable
-            Image.asset(_imagePath, width: 300, height:300),
+            ElevatedButton(onPressed: _showAlertDialog, child: Text("Login")),
           ],
         ),
       ),
